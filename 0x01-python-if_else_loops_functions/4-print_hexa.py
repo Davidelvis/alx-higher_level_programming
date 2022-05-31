58d3ead9ee07e798579b695a8c36c6d1eebea648
Davidelvis <komboelvis08@gmail.com>
2022-05-31 07:16:59 -0700

    For loop

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    print(f"{i} = 0x{i}")#!/usr/bin/python3
-    for i in range(0, 99):
-        print(f"{i} = 0x{i}")#!/usr/bin/python3
-        for i in range(0, 99):
-            print(f"{i} = 0x{i}")
+    print(f"{i} = 0x{i}")
